ec94573b981f6f0341c62c701a197663855917f0
Grzegorz Sieczkowski <grzegorz.sieczkowski@10clouds.com>
2020-01-24 12:59:07 +0100

    place footer elements vertically

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import SocialIcons from "../SocialIcons";
 
 import styles from './footer.module.scss';
+import logo from '../../assets/images/logo.svg';
 
 function Footer() {
 
@@ -10,11 +11,12 @@ function Footer() {
         <footer className={styles.Footer}>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 text-center py-5 d-flex justify-content-center align-items-center">
-                        <div>Copyright &copy; {(new Date()).getFullYear().toString()}</div>
+                    <div className="col-12 text-center py-4 d-flex flex-column justify-content-between align-items-center">
+                        <div className={styles.logo}><img src={logo} alt="React Pizza"/></div>
                         <ul className={styles.social}>
                             <SocialIcons/>
                         </ul>
+                        <div>Copyright &copy; {(new Date()).getFullYear().toString()}</div>
                     </div>
                 </div>
             </div>
